1b78c8c5de9876c3f03e8fe17cb2f6194a8e38bb
Fletcher Chapin <fletchapin@gmail.com>
2020-09-24 12:23:30 -0700

    Removing superfluous static dirs

--- aide_doc/aide_doc/settings.py
+++ aide_doc/aide_doc/settings.py
@@ -118,9 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "form_submit/templates/docs/_static"),
-    "/home/polls.com/polls/static",
-    "/opt/webfiles/common",
+    os.path.join(BASE_DIR, "form_submit/templates/docs/_static")
 ]
 
 STATIC_URL = '/_static/'
