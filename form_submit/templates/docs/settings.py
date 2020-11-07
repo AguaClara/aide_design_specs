f7492a74cc391b1ce84a80e2119ca671414c6754
Fletcher Chapin <fletchapin@gmail.com>
2020-11-06 17:15:40 -0800

    Using versioned model in settings.py

--- form_submit/templates/docs/settings.py
+++ form_submit/templates/docs/settings.py
@@ -1,2 +1,2 @@
 language = 'es'
-link = 'https://cad.onshape.com/documents/c3a8ce032e33ebe875b9aab4/w/1c76ba1fbb5d2b8661c1979e/e/d75b2f7a41dde39791b154e8'
+link = 'https://cad.onshape.com/documents/c3a8ce032e33ebe875b9aab4/v/dc76b3f674d3d5d4f6237f35/e/d75b2f7a41dde39791b154e8'
