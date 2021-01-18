6f9544d4b5913549bc0e5a077c8178bb82553221
Fletcher Chapin <fletchapin@gmail.com>
2021-01-18 09:52:51 -0800

    Reformatted with black and flake8

--- form_submit/urls.py
+++ form_submit/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
-    path('', views.submit_form, name="form"),
-    path('index', views.index),
-    path('<slug:doc>/<slug:page>.html', views.docs),
-] + static('_static', document_root=settings.STATIC_ROOT)
-urlpatterns += static('_images', document_root=settings.MEDIA_ROOT)
+    path("", views.submit_form, name="form"),
+    path("index", views.index),
+    path("<slug:doc>/<slug:page>.html", views.docs),
+] + static("_static", document_root=settings.STATIC_ROOT)
+urlpatterns += static("_images", document_root=settings.MEDIA_ROOT)
