c857269146f483d5be41b1bd573b853114d48932
Fletcher Chapin <fletchapin@gmail.com>
2021-01-18 11:04:17 -0800

    Admin/gh action setup (#46)
    * Updated get_parsed_measurements() call with processes returned
    
    * Created GH Issue and PR templates
    
    * Trying to move from Travis to GH actions
    
    * Reformatted with black and flake8
    
    * Config change
    
    * Reintroducing Travis to pass PR

--- form_submit/apps.py
+++ form_submit/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class FormSubmitConfig(AppConfig):
-    name = 'form_submit'
+    name = "form_submit"
--- form_submit/templates/docs/setup.py
+++ form_submit/templates/docs/setup.py
@@ -2,32 +2,39 @@
 from sphinx.setup_command import BuildDoc
 from setuptools import setup
 
-name = 'AIDE Design Specifications'
-version = '0.0'
-release = '0.0.0'
-cmdclass = {'build_latex': BuildDoc, 'build_html': BuildDoc, }
+name = "AIDE Design Specifications"
+version = "0.0"
+release = "0.0.0"
+cmdclass = {
+    "build_latex": BuildDoc,
+    "build_html": BuildDoc,
+}
 
 setup(
     name=name,
-    author='AguaClara Cornell',
-    description=('Automated documentation builder for '
-                 'the AguaClara Infrastructure Design Engine'),
-    url='https://github.com/AguaClara/aide_design_specs',
+    author="AguaClara Cornell",
+    description=(
+        "Automated documentation builder for "
+        "the AguaClara Infrastructure Design Engine"
+    ),
+    url="https://github.com/AguaClara/aide_design_specs",
     version=release,
     cmdclass=cmdclass,
     python_requires="==3.8",
     # add requirements, e.g. alabaster
-
     # these are optional and override conf.py settings
     command_options={
-        'build_latex': {
-            'project': ('setup.py', name),
-            'version': ('setup.py', version),
-            'release': ('setup.py', release),
-            'builder': ('setup.py', 'latex')},
-        'build_html': {
-            'project': ('setup.py', name),
-            'version': ('setup.py', version),
-            'release': ('setup.py', release),
-            'builder': ('setup.py', 'html')}}
+        "build_latex": {
+            "project": ("setup.py", name),
+            "version": ("setup.py", version),
+            "release": ("setup.py", release),
+            "builder": ("setup.py", "latex"),
+        },
+        "build_html": {
+            "project": ("setup.py", name),
+            "version": ("setup.py", version),
+            "release": ("setup.py", release),
+            "builder": ("setup.py", "html"),
+        },
+    },
 )
