e344ab3d07169d10f4e607a84c05ee845c5f073f
Fletcher Chapin <fletchapin@gmail.com>
2021-02-11 13:44:49 -0800

    Reformatted and linted

--- form_submit/views.py
+++ form_submit/views.py
@@ -6,15 +6,9 @@ import os
 from pdflatex import PDFLaTeX
 from aide_doc import settings
 
-LANGUAGES = [
-    ('es', 'Español'),
-    ('en', 'English')
-]
+LANGUAGES = [("es", "Español"), ("en", "English")]
 
-FILE_TYPE = [
-    ('website', 'Website'),
-    ('pdf', 'PDF')
-]
+FILE_TYPE = [("website", "Website"), ("pdf", "PDF")]
 
 
 class DocGenForm(forms.Form):
@@ -27,14 +21,14 @@ class DocGenForm(forms.Form):
 
 
 def submit_form(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = DocGenForm(request.POST)
         if form.is_valid():
             link = form.cleaned_data["link"]
             language = form.cleaned_data["language"]
             file_type = form.cleaned_data["file_type"]
 
-            if (os.path.basename(os.getcwd()) != "docs"):
+            if os.path.basename(os.getcwd()) != "docs":
                 os.chdir("form_submit/templates/docs")
             # TODO: add 'make clean' equivalent before 'build_sphinx'
 
@@ -43,23 +37,23 @@ def submit_form(request):
             f.write("link = '" + link + "'\n")
             f.close()
 
-            if file_type == 'website':
-                subprocess.call(['python', 'setup.py', 'build_html'])
-                return HttpResponseRedirect('/index')
+            if file_type == "website":
+                subprocess.call(["python", "setup.py", "build_html"])
+                return HttpResponseRedirect("/index")
 
-            elif file_type == 'pdf':
-                subprocess.call(['python', 'setup.py', 'build_latex'])
+            elif file_type == "pdf":
+                subprocess.call(["python", "setup.py", "build_latex"])
                 os.chdir("./build/sphinx/latex")
-                pdfl = PDFLaTeX.from_texfile('AideDesignSpecs.tex')
+                pdfl = PDFLaTeX.from_texfile("AideDesignSpecs.tex")
                 pdf, log, _ = pdfl.create_pdf(keep_pdf_file=True)
 
-                output_file = open('AideDesignSpecs.pdf', 'rb')
+                output_file = open("AideDesignSpecs.pdf", "rb")
 
-                response = HttpResponse(output_file,
-                                        content_type='application/pdf')
+                response = HttpResponse(output_file, content_type="application/pdf")
 
-                response['Content-Disposition'] = ('attachment; filename='
-                                                   '"AideDesignSpecs.pdf"')
+                response["Content-Disposition"] = (
+                    "attachment; filename=" '"AideDesignSpecs.pdf"'
+                )
 
                 output_file.close()
 
@@ -70,22 +64,17 @@ def submit_form(request):
 
         else:
             # If the form is invalid, re-render the page with existing info
-            return render(request, 'form_submit/submit.html', {
-                "form": form
-            })
+            return render(request, "form_submit/submit.html", {"form": form})
 
-    return render(request, 'form_submit/submit.html', {
-        'form': DocGenForm()
-    })
+    return render(request, "form_submit/submit.html", {"form": DocGenForm()})
 
 
 def index(request):
-    return render(request, 'docs/build/sphinx/html/index.html')
+    return render(request, "docs/build/sphinx/html/index.html")
 
 
 def docs(request, doc=index, page=None):
     if page:
-        return render(request, 'docs/build/sphinx/html/'
-                      + doc + '/' + page + '.html')
+        return render(request, "docs/build/sphinx/html/" + doc + "/" + page + ".html")
     else:
-        return render(request, 'docs/build/sphinx/html/' + page + '.html')
+        return render(request, "docs/build/sphinx/html/" + page + ".html")
