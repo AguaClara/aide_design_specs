e8db63c10527db7b4b327a7fbd680bc2714f3a3d
Fletcher Chapin <fletchapin@gmail.com>
2021-02-11 16:41:49 -0800

    Got index.html to load

--- form_submit/templates/docs/settings.py
+++ form_submit/templates/docs/settings.py
@@ -1,5 +1,2 @@
-language = "es"
-link = (
-    "https://cad.onshape.com/documents/5333641b7d4a50ff8c28cd56/"
-    "w/072d4d899d5d5e4d3a69afbd/e/299e5364fbc39e64dab2ca67"
-)
+language = 'en'
+link = 'https://cad.onshape.com/documents/5333641b7d4a50ff8c28cd56/w/072d4d899d5d5e4d3a69afbd/e/299e5364fbc39e64dab2ca67'
